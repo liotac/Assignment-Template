245c9bb152586e12c601327e8ae0c0cec919fe7b
shrike <williamtrung.le@gmail.com>
2018-02-10 15:10:30 -0500

    Define AST nodes

--- src/tree.h
+++ src/tree.h
@@ -1,45 +1,74 @@
-// Expression Tokens
+#include <stdbool.h>
+
+// ----------------------------------------------------------------------------
+// Enum types
+
+// Token types
 typedef enum {
-    tIDENT;
-    tSTRING;
-    stringlit;
-    tINT;
-    tBOOLEAN;
-    intlit;
-    tFLOAT;
-    floatlit;
-    binaryop;
-    OR;
-    AND;
-    EQL;
-    NEQ;
-    ADD;
-    SUB;
-    MUL;
-    DIV;
-    unaryop;
-    NEG;
-    NOT;
-} TokenType;
+    IDENT,
+    STRING,
+    stringlit,
+    INT,
+    BOOLEAN,
+    intlit,
+    FLOAT,
+    floatlit,
+    binaryop,
+    OR,
+    AND,
+    EQL,
+    NEQ,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    unaryop,
+    NEG,
+    NOT
+} ExprToken;
+
 
 // ----------------------------------------------------------------------------
 // Node structures definitions
 
-// exp_s is an Expression Node
-typedef struct Exp {
-    int Line;
-    TokenType Token ;
-    union {
-        char *String;
-        int Int;
-        double Float;
-        struct {
-            Exp *Lhs;
-            Exp *Rhs;
-        } Binary;
-        Exp *Unary;
-    } Val;
-} Exp;
+// Program is the root Node
+typedef struct PROG {
+    DECL *declarations;
+    STMT *statements;
+} PROG;
 
-Exp *NewExpUnary(TokenType token, void *value);
-Exp *NewExpBinary(TokenType token, Exp *lhs, Exp *rhs);
+// Declaration Node
+typedef struct DECL {
+    union { SYMBOL *identifier;
+            EXPR *expression;
+            struct { DECL *elem; DECL *list; } sequence;
+    }
+} DECL;
+
+// Statement Node
+typedef struct STMT {
+    enum { READ, PRINT, ASSIGN, IF, IFELSE, WHILE } token;
+    union { struct { STMT *elem, STMT *list; } sequence;
+            struct { EXPR *condition; STMT *body; } ifS;
+            struct { EXPR *condition; STMT *ifpart, STMT *elsepart; } ifelseS;
+            struct { EXPR *condition; STMT *body; } whileS;
+            struct { SYMBOL *identifier; EXPR *exp; } assignS;
+            EXPR *rwS;
+    } val;
+} STMT;
+
+
+// Expression Node
+typedef struct EXPR {
+    ExpToken token ;
+    union { char *strE;
+            int intE;
+            double floatE;
+            bool boolE;
+            EXPR *unaryE;
+            struct {EXPR *left; EXPR *right;} binaryE;
+    } val;
+} EXPR;
+
+EXPR *NewExpUnary(TokenType token, void *value);
+EXPR *NewExpBinary(TokenType token, EXPR *lhs, EXPR *rhs);
